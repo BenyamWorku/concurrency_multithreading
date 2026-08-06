57ce451eeb30bf80a992f4ad8bcc1782e0494e37
Worku, Benyam (Leonardo, UK) <benyam.worku@des.grplnk.net>
2026-08-06 17:19:20 +0100

    even vs odd , locked critical region

--- experiment_1.cpp
+++ experiment_1.cpp
@@ -1,60 +1,74 @@
-#include<iostream>
-#include<thread>
-#include<algorithm>
+#include <algorithm>
+#include <atomic>
 #include <cassert>
-#include<functional>
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <thread>
+std::mutex mtx;
 
+// How many times the threads increment the shared counter
+int Calculator(int32_t target, int t) { return static_cast<int>(target / t); }
 
+// What the threads do
+void ExecuteJob(int32_t& sharedCounter, const int n) {
+  for (int i = 0; i < n; ++i) {
+    std::lock_guard<std::mutex> lock(mtx);
+    if (sharedCounter % 2 == 0) {
+      sharedCounter++;
 
-void Incrementor( std::atomic<int> &sharedCounter, const int targetValue, int& executionCount, const int maxPerThread)
-{
-    // int thread1counter=0;
-    // int thread2counter=0;
-    int localIncrements=0;
-
-    
-    //int32_t numberOfThreads = 2;
-  while(sharedCounter < targetValue && localIncrements < maxPerThread) {
-    
-    sharedCounter++;
-    executionCount++;
-    localIncrements++;
-
+    } else if (sharedCounter >= 1 && sharedCounter % 2 == 1) {
+      sharedCounter--;
+    }
+  }
+}
+// Spawn Threads
+template <typename callback>
+std::vector<std::thread> ThreadsSpawner(int numberofThreads, callback task) {
+  std::vector<std::thread> vectorOfThreads;
+  vectorOfThreads.reserve(numberofThreads);
+  for (int i = 0; i < numberofThreads; ++i) {
+    vectorOfThreads.emplace_back(task);
   }
 
+  return vectorOfThreads;
 }
 
+int main() {
+  const int NUMBEROFITERATIONS = 10;
+  int NumberOfThreads = 4;
+  int target = 10000000;
+  int maxPerThread{Calculator(target, NumberOfThreads)};
+  int32_t sharedCounter{0};
+  size_t accumulator{0};
 
-int main ()
-{
-    int thread1ExecutionCounter;
-    int thread2ExecutionCounter;
-    int32_t targetValue = 1000000;
-    int NUMBEROFTHREADS = 2;
-    const int NUMBEROFITERATIONS= 100;
-    int32_t maxPerThread = static_cast<int>(targetValue/NUMBEROFTHREADS);
-   
-    for (int32_t i = 0;i<NUMBEROFITERATIONS;++i) {
-        thread1ExecutionCounter=0;
-        thread2ExecutionCounter=0;
-        std::atomic<int> sharedCounter{0};  
-        std::thread thread2(Incrementor, std::ref(sharedCounter), std::ref(targetValue),std::ref(thread2ExecutionCounter),std::ref(maxPerThread));
-      
-        std::thread thread1(Incrementor, std::ref(sharedCounter), std::ref(targetValue), std::ref(thread1ExecutionCounter),std::ref(maxPerThread));
-
-        thread1.join();
-        thread2.join();
-  
-        if(sharedCounter.load()!=targetValue){
-            std::cout<<"i= "<<i<<", Value of SharedCounter is: "<<sharedCounter<<std::endl;
-            // add : numberofRuns vs NumberOftimes Shared counter overshot/UNDERSHOT then plot
-
-        }
-        
+  for (int32_t i = 0; i < NUMBEROFITERATIONS; ++i) {
+    sharedCounter = 0;
+    // Time Started
+    const auto timestarted{std::chrono::steady_clock::now()};
+    // TO DO: refactor to use thread pools to avoid creating and deleting threads at every iteration
+    auto threadSpawned = ThreadsSpawner(NumberOfThreads, [&](){ExecuteJob(sharedCounter,Calculator(target,NumberOfThreads));});
+    for (auto& mythread : threadSpawned) {
+      if (mythread.joinable()) {
+        mythread.join();
+      }
     }
-        std::cout << "Thread 1 did " << thread1ExecutionCounter << " increments"<<std::endl;
-        std::cout << "Thread 2 did " << thread2ExecutionCounter << " increments"<<std::endl;
 
-    return 0;
+    // Time finished
+    const auto timefinished{std::chrono::steady_clock::now()};
 
+    // accumulator holds the final value of sharedcounter
+    accumulator += sharedCounter;
+    const std::chrono::duration<double> timetaken{timefinished - timestarted};
+    std::cout << "Iteration " << i << " took " << timetaken.count()
+              << std::endl;
+  }
+
+  std::cout << "Sharecounter is now: " << sharedCounter << std::endl;
+  std::cout << "The average shared counter value over " << NUMBEROFITERATIONS
+            << " iterations is " << accumulator / NUMBEROFITERATIONS
+            << std::endl;
+
+  return 0;
 }
